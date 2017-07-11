94b09d2c7253e35a85a8767bb7609df16e796195
harry.akbar <harryakbaram@gmail.com>
2017-07-11 22:16:22 +0700

    Init ProgressBar component

---
+++ app/components/ProgressBar/messages.js
@@ -0,0 +1,13 @@
+/*
+ * ProgressBar Messages
+ *
+ * This contains all the text for the ProgressBar component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.ProgressBar.header',
+    defaultMessage: 'This is the ProgressBar component !',
+  },
+});
