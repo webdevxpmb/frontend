b4f08ace1a6211ba65a446b8e743cf8ef13bb729
harry.akbar <harryakbaram@gmail.com>
2017-07-11 22:27:59 +0700

    Init announcement components (list, item)

---
+++ app/components/AnnouncementItem/messages.js
@@ -0,0 +1,13 @@
+/*
+ * AnnouncementItem Messages
+ *
+ * This contains all the text for the AnnouncementItem component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.AnnouncementItem.header',
+    defaultMessage: 'This is the AnnouncementItem component !',
+  },
+});
---
+++ app/components/AnnouncementList/messages.js
@@ -0,0 +1,13 @@
+/*
+ * AnnouncementList Messages
+ *
+ * This contains all the text for the AnnouncementList component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.AnnouncementList.header',
+    defaultMessage: 'This is the AnnouncementList component !',
+  },
+});
